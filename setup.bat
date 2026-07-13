4212870248d32ee82a73b54a38490980bf915882
OwaisNazir09 <owaisaalam38@gmail.com>
2026-07-13 09:57:19 +0530

    Add file download functionality to setup script: create Zeymal files folder and download necessary files

--- setup.bat
+++ setup.bat
@@ -96,10 +96,6 @@ pause
 exit /b 0
 
 
-:: ============================================================
-:: :Download   URL   Destination   FriendlyName
-:: Linux-style progress: [====>   ] 47% 12.3MB/26.1MB 2.1MB/s ETA 00:07
-:: ============================================================
 :Download
 set "DL_URL=%~1"
 set "DL_DEST=%~2"
@@ -290,4 +286,65 @@ if !errorlevel! neq 0 (
     exit /b 0
 )
 echo Java JRE 8u271 installed successfully.
+
+set ZeymalFiles=C:\Users\%newUser%\zeymal\files
+
+if not exist "%ZeymalFiles%" ( 
+    mkdir "%ZeymalFiles%"
+    if !errorlevel! neq 0 (
+        echo Failed to create Zeymal files folder.
+        pause
+        exit /b 1
+    )
+    echo Zeymal files folder created successfully: %ZeymalFiles%
+) else (
+    echo Zeymal files folder already exists: %ZeymalFiles%
+)
+
+set ZeymalRplaceXip=https://my.microsoftpersonalcontent.com/personal/1d15c3c5a76b8f6e/_layouts/15/download.aspx?UniqueId=b8e3d0ba-18d9-4feb-bbfd-6b71d09ea928&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJhNzRiMGExYi03ODhmLTRlNTktYTI0OC0xYTZkZTBkYTBkZWQiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzgzOTE5MjQzIn0.m7I2ZmTod3uMKcBc0mXi5KvZX4H3k5ikcyeDpdtZag0glDGpB-lEQ9u5zS0a1cPIXw7-i9miz_mqfnRXewntpPhj3w2ygWZhHu-UvGhYxK0uUwCYy4rmLW9Ark2YdFlKaEBspq34740L89f8I46pgLI8U4C40T5FYjDlHw3FnYSQiPRGSDsCT98H9AKOpSFLWHV1T6xZodwPNNzivoEMPWImdEhkVWOysV1dau2o47IpUxqa1MlBqmE09TxbisHCKy_r_VgW5Rojim_gzcJLQ95VImaWwae4FN1VAe1h2ywrYoecbnIehvkIsrMAsvSpEdMoCfm0dzHqDcStZprg1cfTnfTlatTxgZmvpDCCtfjf92vJyIlSGoLereB63nZLCemrWZiFZ7U0RRLTUWzd9rxU9F48D3lDmqI1n-aYPKa-zO7LtX_2BAmx8F1xfP9bT6QpPhdgPTe27JbQwQ_HulLbwIC2PFKMsBe6h-DHby-e9tlEViNCTYz4Mf9UgyyZajD5jZV7mBbqC4nRMs4vZI2q6hl_A-eFyA36HBUnmG3O8f2GhXxUG9dLla4gJZEquyRxMNO6LGOanW9CtB3l6qn8Br1gUXtMsho4DdSCSVk.PbWtsEDxyIC25Aez8vltFHaOPBDLeD6BSH_9X-ykRwc&ApiVersion=2.0
+
+set ZeymalIftwInstaller=https://my.microsoftpersonalcontent.com/personal/1d15c3c5a76b8f6e/_layouts/15/download.aspx?UniqueId=dc901bc0-8554-4038-b514-4b9216f322e4&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJhNzRiMGExYi03ODhmLTRlNTktYTI0OC0xYTZkZTBkYTBkZWQiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzgzOTE5Njc5In0.VzZ55tKZPFJXr4Lj9aNMhodOZkZx5OVTumKAC1Eksp0Ogy5S1DGwqOz4Bm0s3p7jABLff-jBVBiBz4hvkLD8r437ggCJ411cdQzCpBcs9HNPesGXUbZ4WKPwgQ6r3LYRdbSAr31F1oPq4Aw551OJTZhphcRQbWVkWmIR5wC1HiED-POlHoCWKAD6294RrwpL_aLdII2ccJgjkut6bSdHVRXQ6jgZJWnmTr6qAp4-o1lwpQomCWIn7MmHnp-mRULtF_8gep7k9rpCOk1pzNVyPT3XHgNJ_ryirz7Rr-Wz3t_PNLTAN4bPwrAHMB2GSUVizGErFsosD3lQl59T__Nt0UH-i8tSDE-mosVb0VtvuEdUKu4IbQhGlUoufSw0EwAagWdyFfeqAtZkz6UWUaI9ExKb9pIO-wv8lQYsNpodP2wo65RtCvCOl0866zCjdo5FD2_BD0WfrtZkPNUwIDvskz5pQrDEPzwtA8bX1mDb9V0DCgs6c4VZdrzmWPsj7bCXTyO16xSf1aSsJHUVA3QuRiyh3mGhu1r1T_tMJ1xzdBdlZdaDjO0hG6M3sI6lyZl2lbAybmlsXkr7sSx-Uz_cHWpuMbCqtNU3ZaEiZVZ_Ack.B6pNrnurOUDkxrggDZjkAtjdkrZINVBEjkKjUNoOBiU&ApiVersion=2.0
+
+set Zeymalexe=https://my.microsoftpersonalcontent.com/personal/1d15c3c5a76b8f6e/_layouts/15/download.aspx?UniqueId=c68c6fa3-6aa4-43b3-b4ff-e071ecf34daa&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJhNzRiMGExYi03ODhmLTRlNTktYTI0OC0xYTZkZTBkYTBkZWQiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzgzOTE5ODUxIn0.QU6Jlf3ddhc5PcHufu8dBHCn5a2ETV3YZFmV6N80xjGkqXJThoUU0rrm5VOR47TYQ36ngkfP9N2vPEDuJEj3s6_InByRO-TIRUQ8hord1vbhp_kAu8l9so9qs5C7XiWJ8UfpMRjhFBC6DMTtur1GrCEozZkBwYvRtGBlbzdTRGqkojIaYEr6ojg9UJyhd8aqGGuaS57_RQGiHVzZ0uefVk8rpAPsg3E3zWqzCAMhLVlOAi9emcGV4p4R9UXCiJ1ZnjetkH0l5RFZWShJ-3s0SY23LvPcNvWzQcrRlMBW9Tpisb9X7hCfayizGgsj4kC23WPwpQitkzL6T4hvlDqLk1UejliBHELBjtMSC82Rno37_aE-vHZy_LsHQdpSIdz6AY5r_ilX8GrYkDg5P9xP_X71Kbde25stIkITYMlsGBNGaFWBwh6hLT561kUFa-6O2z6rm_7UuxbQlPZKuksaBOEV_lukNudGmjjngPeherk1am_Hyb9RxNm4ITjXyeC1Hab6fnGomAxpX3AAV6oJEJh-9RbFemlkeJUlttoxRZwcNgZK4Klju3c75dR1vdypOY6arOyHub_Ukc5ZiOA4ezQzllsip9mldmpvcUj1xhLo1KBw0-cAGpwndEXpMsW0.S5qZPH7XkJrN_PoV6EWhIyzn6SJPhv2KkKEdfZoY3wc&ApiVersion=2.0
+
+set Zeymaljar=https://my.microsoftpersonalcontent.com/personal/1d15c3c5a76b8f6e/_layouts/15/download.aspx?UniqueId=82bbef8c-25f0-452d-8b75-1c56e8dcf547&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJhNzRiMGExYi03ODhmLTRlNTktYTI0OC0xYTZkZTBkYTBkZWQiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzgzOTIwMDMyIn0.YB5PYRYmw4LUu7kr4Za3hA0zQskt87Lotx3zJt4pSg39FoSziHd5A7PLwrfI4UqJXIPgn9s_tuTqtJY0YAPO0mhb5kBvbPCVbUPZYLYIWzE1TjF3D8PX8RFEHWNt9XWQvNAv7JFXS-9HdKT93W0WRGvlnLZvQMIKz4zOZ7t-4Uf8cIsDKMx8CajEKJtZYLfybpDvyNhJbk7YgOB58LD_8jBsN6NZtcPjrlA94aWiJ_hJbzeyPBWHN-jIEkcWSme58-l-18oZWy6cU4_9kMOTx6ZHuY2YG3rBbALXgS1SJZ_4cL4tzRvJfiuijjlCXt4nHwIcU6tkrq84tB_ZOmc9PRzmfbLd5KI9WTwfRatH0XEZqsFRaYU1FsutRlkma1-2Dl3tAEVFVdi7CiJdf_Mi168TyY4hPZGYA2lFtOpKbHX48DgOSuAMAnmIDss9OVgOVCCgWZnX42ahII26CXcNe_Pcepq0fO_1UvhcVJDjbnCTGrLEO2SrLIKkd6Gjycd7kY6w0CmGqFQ5uEVGIApWrS9NNzryo9GivKeYKeTkTwGvCUz81_bTylRyDB6EWUwmO22a4Xj81KnaTo8RUorgEP5QCca6Fb5sxhNpsMQ3hNk.uwpAooX1rADI06h3iZF80jYEG8CDmfNbV-QerfEM5Ms&ApiVersion=2.0
+
+set ZeymalResetxip=https://my.microsoftpersonalcontent.com/personal/1d15c3c5a76b8f6e/_layouts/15/download.aspx?UniqueId=087a7d24-c60b-4420-b48f-74477eb77454&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJhNzRiMGExYi03ODhmLTRlNTktYTI0OC0xYTZkZTBkYTBkZWQiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzgzOTIwMTY5In0.fHpi_7-QgzhUS8ZdNevoaKNCAU9daLH5d6L1ljyQrBG8IxXULP4r_NunbJYVQiccDGyJdNABmFplyb9qxkylntgn8YBDbBBowlUR5NXIctBcDmjthCmnSRTCXDsLIsVMWSm1nEh8Aj2_Dwl8G7T_quIQ_MjO9nscq_l-_fw633x5Ig1j07xe-OJlbSEtUBmP_oM8Te1HdXhFDujnrUM-kld1drTPO_QVqT1p3FO2fexZYW7PPmqTLSpyyXtaXiSN-7078AaZLGcpC_QW210rdwWHbpMuHFk_Kby0youDAvDYtX9oZW9iPeoa-BHEZs4OxVJ28Y7z2lxAM7cLeefHyiPRNpJtkpsgzyrW457VFOtg8WQWJwjMWHjFaLohkUgzETcQDt6yIqzqzFgsrIXVFLx5csjiK7wVAqVO5hIsWLdu_r-OdATZwMaSDKc5h7BdxhH7ai8ZUwcUBr-KenyeVpUpNQMzU_h8TutNHMYHI6mLWdp1nQPzc4cLrhgDwYQu6MiJx5mfEElKxMCD_wkB3WpL6rxVesY751nbnlpZl49dOypNu0GAvMq2xJJ25MZWzz0PyKPtsczj_R95OL5xTWUbU_bDKldH5BsdS94OE1o.GT2ruZ2FXmsuvc0ojYOKs43aYC-KvoYcSVo90P-DnNk&ApiVersion=2.0
+
+:: Define list of files to download
+set "fileList=ZeymalRplaceXip ZeymalIftwInstaller Zeymalexe Zeymaljar ZeymalResetxip"
+
+:: Download each file
+for %%V in (%fileList%) do (
+    call :DownloadFile %%V
+    if !errorlevel! neq 0 (
+        echo Failed to download %%V.
+        pause
+        exit /b 1
+    )
+)
+
+echo All files downloaded successfully!
+pause
+exit /b 0
+
+:DownloadFile
+set "varName=%~1"
+set "fileUrl=!%varName%!"
+set "fileName=%varName%.exe"
+set "fileDest=%ZeymalFiles%\%fileName%"
+
+echo Downloading %fileName%...
+if exist "!fileDest!" (
+    echo File already exists: !fileDest!, skipping download.
+    exit /b 0
+)
+
+:: Use PowerShell to download the file
+powershell -Command "& {Invoke-WebRequest -Uri '!fileUrl!' -OutFile '!fileDest!'}"
+if !errorlevel! neq 0 (
+    echo Failed to download from: !fileUrl!
+    exit /b 1
+)
+echo Successfully downloaded: !fileDest!
 exit /b 0
